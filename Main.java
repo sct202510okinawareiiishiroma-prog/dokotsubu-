d579a404c9fe0feb53f2afc7c0900edef4237796
sct202510okinawareiiishiroma-prog <sct202510okinawa.reiii.shiroma@gmail.com>
2026-01-24 15:22:32 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,88 @@
+package servlet;
+
+//java標準（データの入れ物やエラー処理）
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//jakarta EE (サーブレットやセッションなどのweb機能)
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+//自作クラス（自分が作ったビジネスロジック）
+import model.Mutter;
+import model.PostMutterLogic;
+import model.User;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException,IOException {
+
+		//  つぶやきリストをアプリケーションスコープから取得
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+		//取得できなかった場合は、つぶやきリストを新規作成して
+		//アプリケーションスコープに保存
+		if (mutterList == null) {
+			mutterList = new ArrayList<>();
+			application.setAttribute("mutterList", mutterList);
+		}
+
+		//ログインしているか確認するため
+		//セッションスコープからユーザー情報を取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		if (loginUser == null) {//ログインしていない場合
+			//リダイレクト
+			response.sendRedirect("index.jsp");
+		} else {//ログイン済みの場合
+
+			//フォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//リクエストパラメーターの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		//入力値チェック
+		if (text != null && text.length() != 0) {
+			//アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application =this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+			
+			//セッションスコープに保存されたユーザー情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+			
+			//つぶやきを作成してつぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(),text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			//アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList",mutterList);
+		}else{
+			//エラーメッセージをリクエストスコープに保存
+			request.setAttribute("errorMsg","つぶやきが入力されていません");
+		}
+		
+		//メイン画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request,response);	
+		}
+}
